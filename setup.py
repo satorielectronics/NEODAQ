728164e5d36da4177061b9d2b0b03a992dbee290
Timothy <moequalitycontrol@gmail.com>
2023-07-30 09:46:39 -0500

    setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='neodaq',
+    version='1.0',
+    description='Neopets Stocks Aggregator',
+    author='satorielectronics',
+    install_requires=[
+        'selenium',
+        'beautifulsoup4',
+        'requests',
+        'numpy',
+        'pandas',
+        'colorama'
+    ],
+)
+
