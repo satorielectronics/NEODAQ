2758e89cc42b0b454491ba6acf61035d118730aa
Jerry James <jerry.bindle.james@gmail.com>
2023-08-10 16:01:26 -0700

    adds basic test that fails

--- notifications.py
+++ notifications.py
@@ -2,6 +2,17 @@ class NotificationGenerator:
     def __init__(self):
         print("hi there")
 
+    def setLimits(self, lo, hi):
+        self.lo = lo
+        self.hi = hi
+
+    def push(self, price):
+        return False
 
 
 ng = NotificationGenerator()
+
+ng.setLimits(40,50)
+result = ng.push(0)
+if not result:
+    print("fail")
