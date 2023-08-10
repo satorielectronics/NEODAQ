b3e513c636b8e135809e08b599d7b7ab4e364393
Jerry James <jerry.bindle.james@gmail.com>
2023-08-10 16:08:59 -0700

    adds test case for hi threshold crossing notifs

--- notifications.py
+++ notifications.py
@@ -13,6 +13,39 @@ class NotificationGenerator:
 ng = NotificationGenerator()
 
 ng.setLimits(40,50)
-result = ng.push(0)
-if not result:
-    print("fail")
+# Start at zero just as a choice
+notify = ng.push(0)
+assert not notify
+
+# First price is between lo and hi
+notify = ng.push(45)
+assert not notify
+
+# A value greater than hi is pushed, generate a notif
+notify = ng.push(55)
+assert notify
+
+# This value is bigger than hi, but we just generated a notif so don't do it again
+# Note: we also probably will want an "all time high" notification, but here
+#       i'm just thinking about crossing the hi threshold
+notify = ng.push(60)
+assert not notify
+
+# Push a value below hi, but above the lo threshold
+notify = ng.push(45)
+assert not notify
+
+# Push a value above hi, but no notif bc we haven't passed below lo
+notify = ng.push(55)
+assert not notify
+
+# Pass below lo, no notif
+notify = ng.push(30)
+assert not notify
+
+# pass the hi threshold after the lo threshold was crossed, generate a notif
+notify = ng.push(55)
+assert notify
+
+
+
