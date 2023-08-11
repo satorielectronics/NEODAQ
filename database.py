b8b29606c9aa86d26102e0810fafea5eb562cc0f
Jerry James <jerry.bindle.james@gmail.com>
2023-08-10 18:39:11 -0700

    gets a list of paths to the csv files
    Signed-off-by: Jerry James <jerry.bindle.james@gmail.com>

--- database.py
+++ database.py
@@ -1,5 +1,21 @@
 from pathlib import Path
 
 p = Path('./records')
-var = [x for x in p.iterdir() if x.is_dir()]
-print(var)
+
+
+def is_db_file(f):
+    return f.name.endswith("csv")
+
+
+def get_files():
+    f = []
+    for x in p.iterdir():
+        if is_db_file(x):
+            f.append(x)
+    return f
+
+
+files = get_files()
+
+i = files[0]
+print(i)
